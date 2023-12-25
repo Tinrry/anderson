4a3a86385c9ce396844672a660737bf65c7ce415
hhzheng <zhenghuanhuan@zhejianglab.com>
2023-12-25 19:16:46 +0800

    adjust code to make train range model with the same random seed, recover the experiments.

--- mlp.py
+++ mlp.py
@@ -2,6 +2,7 @@ import os
 from tqdm import tqdm, trange
 import h5py
 import numpy as np
+import copy
 
 import torch
 import torch.nn as nn
@@ -11,22 +12,21 @@ class MultiLayerP():
     def __init__(self, 
                  network, 
                  loss_function,
-                 chebyshev_model_range, 
-                 logger,
+                 chebyshev_i,
                  scheduler=None, 
                  save_log=None,
-                 save_model=None
+                 save_model=None,
+                 logger=None
                  ) -> None:
         self.network = network
         self.loss_function = loss_function
-        self.chebyshev_model_range = chebyshev_model_range
-        self.logger = logger
+        self.chebyshev_i = chebyshev_i
         self.scheduler = scheduler
         self.hdf5_filename = save_log
         self.model_dir = save_model
+        self.logger = logger
    
     def train(self, optimizer, epochs, train_loader, val_loader=None):
-        for chebyshev_i in self.chebyshev_model_range:
             # creating log
             if val_loader:
                 log_dict = {
@@ -40,15 +40,15 @@ class MultiLayerP():
                     'train_loss_per_batch': [],
                     'train_loss_per_epoch': []
                 }
-            self._train(optimizer, epochs, train_loader, val_loader, chebyshev_i, log_dict)
+            self._train(optimizer, epochs, train_loader, val_loader, log_dict)
             
             if self.hdf5_filename is not None:
-                self._save_log(chebyshev_i, log_dict)
+                self._save_log(log_dict)
             if self.model_dir is not None:
-                self._save_model(chebyshev_i)
+                self._save_model()
             
 
-    def _train(self, optimizer, epochs, train_loader, val_loader, chebyshev_i, log_dict):
+    def _train(self, optimizer, epochs, train_loader, val_loader, log_dict):
         # defining weight initialization function
         def init_weights(Module):
             if isinstance(Module, nn.Conv2d):
@@ -68,7 +68,7 @@ class MultiLayerP():
             once_batch = True
             for x_batch, y_batch, _, _ in tqdm(train_loader, desc=f'epoch {epoch+1} in training', leave=False):
                 y_pred = self.network(x_batch).squeeze()
-                y_batch = y_batch[:, chebyshev_i, :, :].squeeze()
+                y_batch = y_batch[:, self.chebyshev_i, :, :].squeeze()
                     
                 loss = self.loss_function(y_pred, y_batch)
                 # backward
@@ -82,6 +82,7 @@ class MultiLayerP():
                     # we print 前10个样本的当前的预测值n-th order chebyshev alpha作为分析
                     self.logger.debug(f" y_pred : {y_pred[:10].flatten()}")
                     self.logger.debug(f"y_batch: {y_batch[:10].flatten()}")
+                    self.logger.debug(f'x_batch: {x_batch[:10,0,0,0].flatten()}')
                     once_batch = False
             
             if self.scheduler is not None:
@@ -95,7 +96,7 @@ class MultiLayerP():
             # validation
             if val_loader:
                 val_list = []
-                self._inference(val_loader, self.network, chebyshev_i, val_list)
+                self._inference(val_loader, self.network, val_list)
                 log_dict['validate_loss_per_batch'].extend(val_list)
                 validate_loss_mean = np.array(val_list).mean()
                 log_dict['validate_loss_per_epoch'].append(validate_loss_mean)
@@ -104,43 +105,42 @@ class MultiLayerP():
         return log_dict
 
     def test(self, data_loader):
-        for chebyshev_i in self.chebyshev_model_range:
-            test_log_dict={
-                'test_loss_per_batch': [],
-                'test_loss_per_epoch': []
-            }
-            log_list = []
-            if self.model_dir:
-                # this will load state dict to self.network
-                self._load_model(chebyshev_i)
-            
-            model = self.network
-            self._inference(data_loader, model, chebyshev_i, log_list)
-            test_log_dict['test_loss_per_batch'] = log_list
-            self.logger.info(f'test {chebyshev_i:03}')
-            test_loss_mean = np.array(test_log_dict['test_loss_per_batch']).mean()
-            test_log_dict['test_loss_per_epoch'].append(test_loss_mean)
-            self.logger.info(f"Test loss: {test_loss_mean:.10f}") 
+        test_log_dict={
+            'test_loss_per_batch': [],
+            'test_loss_per_epoch': []
+        }
+        log_list = []
+        if self.model_dir:
+            # this will load state dict to self.network
+            self._load_model()
+        
+        model = self.network
+        self._inference(data_loader, model, log_list)
+        test_log_dict['test_loss_per_batch'] = log_list
+        self.logger.info(f'test {self.chebyshev_i:03}')
+        test_loss_mean = np.array(test_log_dict['test_loss_per_batch']).mean()
+        test_log_dict['test_loss_per_epoch'].append(test_loss_mean)
+        self.logger.info(f"Test loss: {test_loss_mean:.10f}") 
 
-            if self.hdf5_filename:
-                self._save_log(chebyshev_i,  test_log_dict)
+        if self.hdf5_filename:
+            self._save_log(test_log_dict)
 
     @torch.no_grad
-    def _inference(self, data_loader, model, chebyshev_i, log_list):
+    def _inference(self, data_loader, model, log_list):
         
         model.eval()
         for x_batch, y_batch, _, _ in tqdm(data_loader, leave=False):
 
             y_pred = model(x_batch).squeeze()
-            y_batch = y_batch[:, chebyshev_i, :, :].squeeze()
+            y_batch = y_batch[:, self.chebyshev_i, :, :].squeeze()
             loss = self.loss_function(y_pred, y_batch)
 
             log_list.append(loss.detach().cpu().item())
         return log_list
 
-    def _save_log(self, chebyshev_i, data_dict):
+    def _save_log(self, data_dict):
         h5_handle = h5py.File(self.hdf5_filename, 'a')
-        model_index = f'model_{chebyshev_i:03}'
+        model_index = f'model_{self.chebyshev_i:03}'
         grp_i = h5_handle.require_group(model_index)
         # save dict in hdf5
         for k, v in data_dict.items():
@@ -149,7 +149,7 @@ class MultiLayerP():
 
     def read_log(self, chebyshev_i=None):
         if chebyshev_i is None:
-            chebyshev_i = self.chebyshev_model_range[0]
+            chebyshev_i = self.chebyshev_i
         h5_handle = h5py.File(self.hdf5_filename, 'r')
         grp_i = h5_handle.require_group(name=f'model_{chebyshev_i:03}')
         train_loss_per_batch = []
@@ -171,20 +171,15 @@ class MultiLayerP():
         return (train_loss_per_batch, train_loss_per_epoch), (validate_loss_per_batch, validate_loss_per_epoch), (test_loss_per_batch, test_loss_per_epoch)
 
 # todo 查看第二个模型分析如何下降loss
-    def _save_model(self, chebyshev_i=None):
-        if chebyshev_i is None:
-            chebyshev_i = self.chebyshev_model_range[0]
-        
+    def _save_model(self):
+ 
         if not os.path.exists(self.model_dir):
             os.mkdir(self.model_dir)
-        save_file = os.path.join(self.model_dir, f"{chebyshev_i:03}.pth")
+        save_file = os.path.join(self.model_dir, f"{self.chebyshev_i:03}.pth")
         torch.save(self.network.state_dict(), save_file)
     
-    def _load_model(self, chebyshev_i):
-        if chebyshev_i is None:
-            chebyshev_i = self.chebyshev_model_range[0]
-        
-        model_name = os.path.join(self.model_dir, f"{chebyshev_i:03}.pth")
+    def _load_model(self):
+        model_name = os.path.join(self.model_dir, f"{self.chebyshev_i:03}.pth")
         checkpoint = torch.load(model_name)
         self.network.load_state_dict(checkpoint)
         try:
--- run_mlp.py
+++ run_mlp.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import logging
+import re
 
 import torch
 import torch.nn as nn
@@ -21,7 +22,7 @@ logger = logging.getLogger(
     " train-mlp"
 )
 # set the logging level
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 # create a console handler and set its level
 console_handler = logging.StreamHandler()
@@ -34,12 +35,9 @@ console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
 
-def main(config_file, network, layer_num=7, loss_file=None, logger=logger):  
-    # hyper-parameters
-    config = load_config(config_file)
-
+def train_model(network, chebyshev_i, layer_num, loss_temp_file):
+    torch.manual_seed(1)
     L, N = config["L"], config["N"]
-    model_range = np.arange(int(config['model_order']))
     epochs = config["n_epoch"]
     lr = config["lr"]
     batch_size = config['batch_size']
@@ -48,10 +46,7 @@ def main(config_file, network, layer_num=7, loss_file=None, logger=logger):
     step_size = config['step_size']
     gamma = config['gamma']
     save_model = config['save_model']
-    # 创建一个文件，保存当前的训练结果，成功后再归档到对应的网络架构资料中。
-    loss_temp_file = 'loss.h5'
-    if os.path.exists(loss_temp_file):
-        os.remove(loss_temp_file)
+    
 
     input_d = L + 2
     device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
@@ -64,7 +59,6 @@ def main(config_file, network, layer_num=7, loss_file=None, logger=logger):
     test_loader = DataLoader(test_set, shuffle=False, batch_size=batch_size)
     val_loader = DataLoader(val_set, shuffle=False, batch_size=batch_size)
 
-    # TODO 改成相对误差平均
     loss_function = nn.MSELoss()
     # every time we save model in train function, and load model in compose_chebyshev_alpha, 256 models
     # loss is too small
@@ -74,17 +68,35 @@ def main(config_file, network, layer_num=7, loss_file=None, logger=logger):
     network.to(device).double()
     model = MultiLayerP(network, 
                         loss_function,
-                        chebyshev_model_range=model_range,
+                        chebyshev_i=chebyshev_i,
                         scheduler=scehduler, 
                         save_log=loss_temp_file,
                         save_model=save_model,
                         logger = logger)
 
-    train_log_dict = model.train(optimizer=optimizer, 
+    model.train(optimizer=optimizer, 
                            epochs=epochs, 
                            train_loader=train_loader, 
                            val_loader=val_loader)
-    test_log_dict = model.test(test_loader)
+    model.test(test_loader)
+
+def main(network, layer_num=7, loss_file=None, logger=logger):  
+    
+    # 创建一个文件，保存当前的训练结果，成功后再归档到对应的网络架构资料中。
+    loss_temp_file = 'loss.h5'
+    if os.path.exists(loss_temp_file):
+        os.remove(loss_temp_file)
+
+    model_list = re.findall(r"\d+", config['model_range'])
+    if len(model_list) == 2:
+        begin, end = int(model_list[0]), int(model_list[1])
+    else:
+        begin, end = 0, int(model_list[0])
+    model_range = np.arange(begin, end)
+
+    for chebyshev_i in model_range:
+        train_model(network, chebyshev_i, layer_num, loss_temp_file)
+    
 
     # 避免训练一半，把原来的文件给删除了。
     if loss_file is None:
@@ -92,9 +104,11 @@ def main(config_file, network, layer_num=7, loss_file=None, logger=logger):
 
     if os.path.exists(loss_file):
         os.remove(loss_file)
-        os.rename(src=loss_temp_file, dst=loss_file)
+    os.rename(src=loss_temp_file, dst=loss_file)
 
 
 if __name__ == '__main__':
-    from nn_models import MyMLP   
-    main('config_3.json', network=MyMLP, layer_num=14)
+    from nn_models import MyMLP  
+    # hyper-parameters
+    config = load_config('config_3_1.json') 
+    main(network=MyMLP, layer_num=14)
